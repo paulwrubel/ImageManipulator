fa6cde31e144ab88b3ab30aa49c3188421fd8317
Paul Wrubel <pjwrubel@mtu.edu>
2018-02-02 15:40:56 -0500

    Initial Commit-ish

---
+++ src/Manager.java
@@ -0,0 +1,9 @@
+import javafx.scene.image.Image;
+
+public class Manager {
+    public static void main(String args[]) {
+        System.out.println("testing");
+
+        Image image = new Image("path/to/image");
+    }
+}
